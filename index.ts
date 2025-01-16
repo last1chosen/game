9dc096bee32563b9aec9dd74ce98145b86ba8b4d
last1chosen <63253895+last1chosen@users.noreply.github.com>
2025-01-16 13:49:45 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+import { isGameOver } from "./functions/isGameOver.ts";
+import { isRemainingMoves } from "./functions/isRemainingMoves.ts";
+import { isWinner } from "./functions/isWinner.ts";
+import { Board } from "./types/.index.ts";
 
+export const evaluateGame = (board: Board): string => {
+    if (checkWinner(board, 'X')) {
+	    return 'X wins'
+    }
+    if (checkWinner(board, 'O')) {
+	    return 'O wins'
+    }
+    if (isRemainingMoves(board)) {
+	    return 'Game is still going'
+    }
+    return 'Game is a draw, no winner and no moves remaining';
+};
