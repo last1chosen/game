6866b49ad450ab62900f89325c14f509a8e29439
last1chosen <63253895+last1chosen@users.noreply.github.com>
2025-01-16 13:53:32 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import { isWinner } from "./functions/isWinner.ts";
 import { Board } from "./types/.index.ts";
 
 export const evaluateGame = (board: Board): string => {
-    if (checkWinner(board, 'X')) {
+    if (isWinner(board, 'X')) {
 	    return 'X wins'
     }
-    if (checkWinner(board, 'O')) {
+    if (isWinner(board, 'O')) {
 	    return 'O wins'
     }
     if (isRemainingMoves(board)) {
