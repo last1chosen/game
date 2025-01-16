ff43eaaabdfc55814b5419d5ea97291ee020a7f0
last1chosen <63253895+last1chosen@users.noreply.github.com>
2025-01-16 13:47:10 -0500

    Update isRemainingMoves.ts

--- functions/isRemainingMoves.ts
+++ functions/isRemainingMoves.ts
@@ -1,5 +1,5 @@
 import { Board } from "../types/index.ts";
 
-const isRemainingMoves = (board: Board): boolean => {
+export const isRemainingMoves = (board: Board): boolean => {
     return board.some(row => row.some(cell => cell === null))
 }
